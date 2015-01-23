f94c4fbed527a93c7460291f51eedddba0107855
Jack Schlotthauer <jack@sosh.com>
2015-01-23 15:06:36 -0800

    added in some grunt/protractor tools to our packages.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,57 @@
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-karma');
+    grunt.initConfig({
 
-  grunt.initConfig({
+        'pkg': grunt.file.readJSON('package.json'),
 
-    'pkg': grunt.file.readJSON('package.json'),
+        'meta': {
+            'jsFilesForTesting': [
+                'test/**/*Spec.js'
+            ]
+        },
 
-    'meta': {
-      'jsFilesForTesting': [
-        'test/**/*Spec.js'
-      ]
-    },
+        'karma': {
+            'development': {
+                'configFile': 'karma.conf.js',
+                'options': {
+                    'files': [
+                        '<%= meta.jsFilesForTesting %>',
+                        'source/**/*.js'
+                    ]
+                }
+            }
+        },
 
-    'karma': {
-      'development': {
-        'configFile': 'karma.conf.js',
-        'options': {
-          'files': [
-            '<%= meta.jsFilesForTesting %>',
-            'source/**/*.js'
-          ],
+        protractor: {
+            options: {
+                keepAlive: true,
+                configFile: "protractor.conf.js"
+            },
+            singlerun: {},
+            auto: {
+                keepAlive: true,
+                options: {
+                    args: {
+                        seleniumPort: 4444
+                    }
+                }
+            }
+        },
+
+        'jshint': {
+            'myFiles': ['source/*.js']
         }
-      }
-    },
 
-    'jshint': {
-      'myFiles': ['source/*.js']
-    }
+    });
 
-  });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-protractor-runner');
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('unit', ['karma:development']);
-  grunt.registerTask('lint', ['jshint:myFiles']);
+    grunt.registerTask('default', ['jshint:myFiles']);
+    grunt.registerTask('unit', ['karma:development']);
+    grunt.registerTask('e2e', ['protractor:singlerun']);
+    grunt.registerTask('test', ['karma:development','protractor:singlerun']);
+    grunt.registerTask('lint', ['jshint:myFiles']);
 
 };
