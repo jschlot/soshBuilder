1b64d6d4dfdefc8f506710f810558bfafd545e6c
Jack Schlotthauer <jack.schlotthauer@gmail.com>
2015-02-07 00:05:10 -0800

    updating src direction

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 preferred_syntax = :sass
 http_path = '/'
 css_dir = 'pipeline/public/stylesheets'
-sass_dir = 'pipeline/public/sass'
+sass_dir = 'pipeline/public/assets'
 images_dir = 'assets/images'
 javascripts_dir = 'pipeline/public/javascripts'
 relative_assets = true
