039d9c19a618b46efff90e2b2ba6594a7cadaf0f
Jack Schlotthauer <jack@sosh.com>
2015-02-06 17:14:08 -0800

    install of sass and of 'home page' API mock

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'pipeline/public/stylesheets'
+sass_dir = 'pipeline/public/sass'
+images_dir = 'assets/images'
+javascripts_dir = 'pipeline/public/javascripts'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
