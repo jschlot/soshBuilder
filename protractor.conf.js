e3758fdc9e64c4f44a264678e2e242f0f1aee126
Jack Schlotthauer <jack@sosh.com>
2015-02-19 10:57:17 -0800

    updated protractor conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -15,14 +15,16 @@ exports.config = {
     suites: {
         smoke: [
             'test/e2e/*-spec.js',
-            'test/e2e/checkout-flow/*-spec.js'
+            'test/e2e/checkout-flow/*-spec.js',
+            'test/e2e/promo/*-spec.js'
         ],
         login: 'test/e2e/login-spec.js',
+        home: 'test/e2e/home-spec.js',
         promo: [
-            // 'test/e2e/promo/init-spec.js'
+            'test/e2e/promo/init-spec.js',
             'test/e2e/promo/series-spec.js'
         ],
-        home: 'test/e2e/home-spec.js',
+        promo_series: 'test/e2e/promo/series-spec.js',
         checkout: [
             'test/e2e/checkout-flow/init-spec.js',
             'test/e2e/checkout-flow/ticketing-spec.js',
@@ -30,9 +32,6 @@ exports.config = {
             'test/e2e/checkout-flow/payment-spec.js',
             'test/e2e/checkout-flow/purchase-spec.js'
         ],
-        init: 'test/e2e/checkout-flow/init-spec.js',
-        payment: 'test/e2e/checkout-flow/payment-spec.js',
-        purchase: 'test/e2e/checkout-flow/purchase-spec.js',
         ticketing: [
             'test/e2e/checkout-flow/ticketing-spec.js',
             'test/e2e/checkout-flow/ticketing-single-option-spec.js'
